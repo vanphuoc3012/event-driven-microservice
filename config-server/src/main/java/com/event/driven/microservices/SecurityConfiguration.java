ff78a84407274b941ac23c44dbd6ecbd0a71d705
vanphuoc <vanphuoc3012@gmail.com>
2023-09-19 00:49:03 +0700

    encrypt password

---
+++ config-server/src/main/java/com/event/driven/microservices/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.event.driven.microservices;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/encrypt/**").ignoringAntMatchers("/decrypt/**");
+
+//        http.authorizeRequests()
+//            .antMatchers("/encrypt/**")
+//            .authenticated()
+//            .antMatchers("/decrypt/**")
+//            .authenticated();
+        return http.build();
+    }
+}
